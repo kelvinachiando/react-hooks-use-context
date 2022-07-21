bd54d03cd7879da993091f314338d338904884c5
kelvinachiando <kelvinostin22gmail.com>
2022-07-22 00:13:17 +0300

    Done.

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
 
-function Interests({ interests, theme }) {
+function Interests({ interests }) {
+  const { theme } = useContext(ThemeContext);
   return (
     <div>
       <h4>Interests</h4>
@@ -12,5 +14,4 @@ function Interests({ interests, theme }) {
     </div>
   );
 }
-
 export default Interests;
